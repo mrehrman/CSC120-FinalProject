ac2c040272c94c55e101949f9a2e7dc5200fb131
mrehrman <mrehrman@smith.edu>
2024-12-02 20:02:18 -0500

    update loop and interface

--- GameInterface.java
+++ GameInterface.java
@@ -2,8 +2,24 @@ public class GameInterface{
 
     public static void main(String[] args) {
 
+        //initiate protagonist
+        Character protagonist = new Character("Protagonist", "You are exploring the enchanted forest.", 7.5, 7.5);
+        System.out.println(protagonist.description);
+ 
+        //create map
+        GameMap gameMap = new GameMap();
+        Location waterfall = new Location("Waterfall", "There is a large waterfall in front of you.", 2.5, 7.5);
+        Location cottage = new Location("Cottage", "You are inside of a small wooden cottage. \n You can smell cookies baking and see a cozy fire burning in the fireplace.", 7.5, 7.5);
+        Location forest1 = new Location("Forest", "You are in the forest. There are tall trees around. The sun shines through the trees.", 12.75, 7.5);
+        Location forest2 = new Location("Forest on east edge of the map", "You are in the forest. You cannot move any farther east.", 17.5, 7.5);
+        gameMap.addLocation(waterfall);
+        gameMap.addLocation(cottage);
+        gameMap.addLocation(forest1);
+        gameMap.addLocation(forest2);
+        System.out.println(gameMap);
+
         GameLoop playGame = new GameLoop();
-        playGame.loop();
+        playGame.loop(protagonist);
         
     }
 }
--- GameLoop.java
+++ GameLoop.java
@@ -28,24 +28,8 @@ public class GameLoop{
     /**
      * Loops through the game
      */
-    public void loop(){
-        //initiate protagonist
-        Character protagonist = new Character("Protagonist", "You are exploring the enchanted forest.", 7.5, 7.5);
-        System.out.println(protagonist.description);
-
-        //create map
-        GameMap gameMap = new GameMap();
-        Location waterfall = new Location("Waterfall", "There is a large waterfall in front of you.", 2.5, 7.5);
-        Location cottage = new Location("Cottage", "You are inside of a small wooden cottage. \n You can smell cookies baking and see a cozy fire burning in the fireplace.", 7.5, 7.5);
-        Location forest1 = new Location("Forest", "You are in the forest. There are tall trees around. The sun shines through the trees.", 12.75, 7.5);
-        Location forest2 = new Location("Forest on east edge of the map", "You are in the forest. You cannot move any farther east.", 17.5, 7.5);
-        gameMap.addLocation(waterfall);
-        gameMap.addLocation(cottage);
-        gameMap.addLocation(forest1);
-        gameMap.addLocation(forest2);
-        System.out.println(gameMap);
+    public void loop(Character protagonist){
         
-        //loop
         System.out.println("Welcome to the library. Your task is to retrieve the stolen books.");
         System.out.println("Enter anything to play. You can move 'NORTH, SOUTH, EAST, or WEST.' \n Enter 'QUIT' to exit the game.");
         while (this.playing) {
