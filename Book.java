53b75cdf18f061211fd9dda88ac5df3e0a1f3241
mrehrman <mrehrman@smith.edu>
2024-12-09 16:23:16 -0500

    update javadoc comments

--- Book.java
+++ Book.java
@@ -1,3 +1,7 @@
+/**
+ * A type of GameObject that cna hold spells to affect Character abilities.
+ * Note: this could become an instance of the GameObject class instead of its own class.
+ */
 public class Book extends GameObject {
 
     protected boolean swimSpell; 
